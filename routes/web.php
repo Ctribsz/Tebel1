a34bfba88e399fd45072b2e0d614de8f1d3ff862
Ana Tschen <tsc221645@uvg.edu.gt>
2024-04-18 16:32:29 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
-
+use App\Http\Controllers\ProductosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('inicio', [InicioController::class, 'index']);
-
+Route::get('productos',[ProductosController::class, 'index'] );
 
