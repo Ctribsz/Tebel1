aac20f0e1e39df3b93cf76dae303efa511dd7635
Ctribsz <122636369+Ctribsz@users.noreply.github.com>
2024-04-19 21:15:25 -0600

    view incio created

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,120 @@
+@extends('templates.frontend')
+
+@section('content')
+<div class="pd-ltr-20 xs-pd-20-10">
+				<div class="min-height-200px">
+					<div class="page-header">
+						<div class="row">
+							<div class="col-md-6 col-sm-12">
+								<div class="title">
+									<div class="col-md-8">
+									<h4 class="font-20 weight-500 mb-10 text-capitalize">
+										Promociones
+										<div class="weight-600 font-30 text-blue">Mes de enero</div>
+									</h4>
+									<p class="font-18 max-width-600">
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
+										hic non repellendus debitis iure, doloremque assumenda. Autem
+										modi, corrupti, nobis ea iure fugiat, veniam non quaerat
+										mollitia animi error corporis.
+									</p>
+									</div>	
+								</div>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="title pb-20 pt-20">
+					<h2 class="h3 mb-0">Categoria de productos</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 mb-20">
+					<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/clean-category.svg" alt="" />
+							</div>
+						<div class="content">
+							<h3 class="h4">Limpieza</h3>
+							<p class="max-width-200">
+								Los mejores productos para limpieza del hogar
+							</p>
+						</div>
+					</a>
+					</div>
+					<div class="col-md-4 mb-20">
+						<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/car-category.svg" alt="" />
+							</div>
+							<div class="content">
+								<h3 class="h4">Automotriz</h3>
+								<p class="max-width-200">
+									Los mejores productos para limpieza de vehiculos
+								</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 mb-20">
+						<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/school-category.svg" alt="" />
+							</div>
+							<div class="content">
+								<h3 class="h4">Categoria 3</h3>
+								<p class="max-width-200">
+									Informacion de la categoria 3
+								</p>
+							</div>
+						</a>
+				</div>
+			</div>
+
+			<div class="title pb-20 pt-20">
+					<h2 class="h3 mb-0">Atencion al cliente</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 mb-20">
+					<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/phone-category.svg" alt="" />
+							</div>
+						<div class="content">
+							<h3 class="h4">WhatsApp</h3>
+							<p class="max-width-200">
+								
+							</p>
+						</div>
+					</a>
+					</div>
+					<div class="col-md-4 mb-20">
+						<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/phone-category.svg" alt="" />
+							</div>
+							<div class="content">
+								<h3 class="h4">Número telefonico</h3>
+								<p class="max-width-200">
+									
+								</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-4 mb-20">
+						<a href="#" class="card-box d-block mx-auto pd-20 text-secondary">
+							<div class="img pb-30">
+								<img src="back/vendors/images/phone-category.svg" alt="" />
+							</div>
+							<div class="content">
+								<h3 class="h4">Correo Electronico</h3>
+								<p class="max-width-200">
+									
+								</p>
+							</div>
+						</a>
+				</div>
+			</div>
+@endsection
