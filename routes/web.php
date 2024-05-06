e3a85854b3345cdb85deb3dee5c568d564d1bdf5
Ana Tschen <tsc221645@uvg.edu.gt>
2024-05-06 15:09:33 -0600

    comienzo ruta y vista mision y vision

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\Login2Controller;
+use App\Http\Controllers\MisionVisionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,4 @@ Route::get('/', function () {
 Route::get('inicio', [InicioController::class, 'index']);
 Route::get('productos',[ProductosController::class, 'index']);
 Route::get('login',[Login2Controller::class, 'index']);
+Route::get('misionvision',[MisionVisionController::class, 'index']);
