b1c07a12df522208681c2e5887cc251959eb4dfe
Ana Tschen <tsc221645@uvg.edu.gt>
2024-05-23 21:31:39 -0600

    primeros productos - precios

--- database/seeders/precios_productos.php
+++ database/seeders/precios_productos.php
@@ -12,6 +12,248 @@ class precios_productos extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = [
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 8 OZ UNIVERSAL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 10.91638888888889,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 10 OZ UNIVERSAL 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 12.270416666666668,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 12 OZ UNIVERSAL DOT3',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 13.999583333333334,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 32 OZ UNIVERSAL DOT3',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 35.91583333333333,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 1 GL UNIVERSAL DOT 3',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 140.165,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 8 OZ CENTURY 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 11.895416666666668,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 12 OZ CENTURY 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 15.29125,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 12 OZ CENTURY 2001 12U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 15.332500000000001,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIQUIDO FRENOS 32 OZ CENTURY 2001',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 38.5825,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 32 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 9.395416666666668,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 1GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 27.748333333333335,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 3 GL 2U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 72.995,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 1 GL DIESEL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 29.498333333333335,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 3 GL DIESEL 2U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 72.995,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL VERDE 5 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 115.99,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL ROJO 32 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 9.937083333333334,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL ROJO 1GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 30.748333333333335,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL ROJO 3 GL 2U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 81.995,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL ROJO 5 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 133.49,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL 33% 1 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 73.83166666666666,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL 33% 5 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 344.49,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL 50% 1 GL PREMIUM',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 101.915,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'CAR KOOL 50% 5 GL PREMIUM',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 481.49,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PROD ACTIVAD ELECT CONC 1250 500ML (16OZ)',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 8.999583333333334,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+        ];
+        DB::table('products')->insert($products);
     }
 }
