a6bd11cd5fbb3839cd01e3a20b9e9c2c7925b871
Ana Tschen <tsc221645@uvg.edu.gt>
2024-05-23 21:32:31 -0600

    segunda mitad - productos

--- database/seeders/precios_productos.php
+++ database/seeders/precios_productos.php
@@ -253,6 +253,266 @@ class precios_productos extends Seeder
                 'popular' => false,
             ],
 
+            [
+                'name' => 'PRODIN ACTIVADOR ELECTROLITICO 18 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 6.291250000000001,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PROD ACTIVAD ELECT CONC 1250 700ML (25OZ)',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 10.177857142857144,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN ACTIVADOR ELECTROLITICO 32 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 10.3325,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PROD ACTIVAD ELECT CONC 1250 960ML (32OZ)',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 16.915833333333335,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN ACTIVADOR ELECTROLITICO 1 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 27.831666666666667,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN ACTIVADOR ELECT CONC 1250 1 GL 4U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 45.1225,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN AGUA DESTILADA 18 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 6.145416666666667,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN AGUA DESTILADA 32 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 8.749166666666666,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN AGUA DESTILADA 1GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 22.998333333333335,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIMPIA INYECTORES GASOLINA 12 OZ 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 19.520416666666666,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN LIMPIA INYECTORES DIESEL 12 OZ 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 19.645416666666666,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'PRODIN PISTON PROTECTOR 16 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 43.58291666666667,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR BLANCO 8 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 27.35375,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR ROJO 8 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 27.35375,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR BLANCO 1 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 188.3725,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR ROJO 1 GL',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 188.3725,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR ROJO 320GR H2O BOLSA',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 8.582916666666668,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR BLANCO 320GR H2O BOLSA',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 8.582916666666668,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR ROJO 320 GR H2O 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 14.624583333333334,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR BLANCO 320GR H2O 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 14.437083333333334,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR ROJO 1GL H2O',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 132.2475,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR BLANCO 1GL H2O',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 129.7475,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR LIQUIDO 8 OZ',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 20.8325,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL PULIDOR LIQUIDO 16 OZ 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 40.97875,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL CERA AMARILLA 8 OZ 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 45.27041666666667,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
+            [
+                'name' => 'VISOL CERA AZUL 8 OZ 24U',
+                'url_products' => '', // Valor nulo para 'url_products'
+                'description' => 'Excede especificaciones DOT 3 y SAE 70R1/70R3 para líquidos de frenos.',
+                'price' => 53.45791666666667,
+                'category_id' => 3,
+                'line_id' => '', // Valor nulo para 'line_id'
+                'popular' => false,
+            ],
+
         ];
         DB::table('products')->insert($products);
     }
